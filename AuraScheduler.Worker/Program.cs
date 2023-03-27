2864640cd8598e33229c2b8a69a898bf69119b21
Jimmie Houts <Jimmie@teamredwall.com>
2023-03-27 09:54:39 -0700

    Added Windows Service Installer #2

--- AuraScheduler.Worker/Program.cs
+++ AuraScheduler.Worker/Program.cs
@@ -3,12 +3,41 @@ using AuraScheduler.Worker;
 using Microsoft.Extensions.Logging.Configuration;
 using Microsoft.Extensions.Logging.EventLog;
 
+
+using CliWrap;
+
+const string ServiceName = "Aura Scheduler Service";
+
+if (args is { Length: 1 })
+{
+    string executablePath = Path.Combine(AppContext.BaseDirectory, "AuraScheduler.Worker.exe");
+
+    if (args[0] is "/Install")
+    {
+        await Cli.Wrap("sc")
+            .WithArguments(new[] { "create", ServiceName, $"binPath={executablePath}", "start=auto" })
+            .ExecuteAsync();
+    }
+    else if (args[0] is "/Uninstall")
+    {
+        await Cli.Wrap("sc")
+            .WithArguments(new[] { "stop", ServiceName })
+            .ExecuteAsync();
+
+        await Cli.Wrap("sc")
+            .WithArguments(new[] { "delete", ServiceName })
+            .ExecuteAsync();
+    }
+
+    return;
+}
+
 IHost host = Host.CreateDefaultBuilder(args)
                  .ConfigureServices((context, services) =>
                  {
                      services.AddWindowsService(options =>
                      {
-                         options.ServiceName = "Aura Scheduler Service";
+                         options.ServiceName = ServiceName;
                      });
 
                      // See: https://github.com/dotnet/runtime/issues/47303
