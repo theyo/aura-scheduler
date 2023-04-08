fe5ab37984e6590db2a315d0c86fa074abf39431
Jimmie Houts <Jimmie@teamredwall.com>
2023-04-08 16:00:23 -0700

    Changed to WiX installer Added windows sandbox configuration (not tested yet)

--- AuraScheduler.Worker/Program.cs
+++ AuraScheduler.Worker/Program.cs
@@ -1,41 +1,9 @@
-using System.Runtime.InteropServices;
-
-using AuraScheduler.Worker;
-
-using CliWrap;
-
 using Microsoft.Extensions.Logging.Configuration;
 using Microsoft.Extensions.Logging.EventLog;
 
+using AuraScheduler.Worker;
+
 const string ServiceName = "AURA Scheduler Service";
-
-if (args is { Length: 1 })
-{
-    string executablePath = Path.Combine(AppContext.BaseDirectory, "AuraScheduler.Worker.exe");
-
-    if (args[0] is "/Install")
-    {
-        await Cli.Wrap("sc")
-                 .WithArguments(new[] { "create", ServiceName, $"binPath={executablePath}", "start=auto" })
-                 .ExecuteAsync();
-
-        await Cli.Wrap("sc")
-             .WithArguments(new[] { "start", ServiceName })
-             .ExecuteAsync();
-    }
-    else if (args[0] is "/Uninstall")
-    {
-        await Cli.Wrap("sc")
-                 .WithArguments(new[] { "stop", ServiceName })
-                 .ExecuteAsync();
-
-        await Cli.Wrap("sc")
-                 .WithArguments(new[] { "delete", ServiceName })
-                 .ExecuteAsync();
-    }
-
-    return;
-}
 
 var builder = Host.CreateApplicationBuilder(args);
 
@@ -44,7 +12,7 @@ if (builder.Environment.IsProduction())
 {
 
     var appDataRoot = Environment.GetFolderPath(Environment.SpecialFolder.CommonApplicationData);
-    var settingsPath = Path.Combine(appDataRoot, "TheYo", "Aura Scheduler", "LightSettings.json");
+    var settingsPath = Path.Combine(appDataRoot, "TheYo", "AURA Scheduler", "LightSettings.json");
 
     builder.Configuration.AddJsonFile(settingsPath, true, true);
 }
@@ -62,7 +30,6 @@ if (OperatingSystem.IsWindows())
 
 builder.Services.Configure<LightOptions>(builder.Configuration.GetSection(LightOptions.SectionName));
 builder.Services.AddHostedService<AuraScheduleWorker>();
-
 
 IHost host = builder.Build();
 
---
+++ InstallerSandboxStart.cmd
@@ -0,0 +1,4 @@
+start appwiz.cpl
+start services.msc
+start "" "C:\source\Setup\bin\x64\Release"
+start "" "C:\Program Files"
