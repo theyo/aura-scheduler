e7d4db7bd72f5d64b68318682e3f3cdc37a61642
Jimmie Houts <Jimmie@teamredwall.com>
2023-04-15 18:55:30 -0700

    Moved all startup logic to new program.cs

--- AuraScheduler.UI/App.xaml.cs
+++ AuraScheduler.UI/App.xaml.cs
@@ -19,78 +19,17 @@ namespace AuraScheduler.UI
     /// </summary>
     public partial class App : Application
     {
-        private const string LightSettingsFileName = "LightSettings.json";
-
-        private readonly IHost _host;
-
-        public App(string[]? args = null)
+        public App()
         {
-
-            var builder = Host.CreateApplicationBuilder(args);
-
-            string settingsPath;
-
-            if (builder.Environment.IsProduction())
-            {
-                var company = typeof(App).Assembly.GetCustomAttribute<AssemblyCompanyAttribute>()!.Company;
-                var product = typeof(App).Assembly.GetCustomAttribute<AssemblyProductAttribute>()!.Product;
-                var appDataRoot = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
-
-                settingsPath = Path.Combine(appDataRoot, company, product, LightSettingsFileName);
-            }
-            else
-            {
-                settingsPath = LightSettingsFileName;
-            }
-
-
-            builder.Configuration.AddJsonFile(settingsPath, true, true);
-
-            builder.Services.Configure<LightOptions>(builder.Configuration.GetSection(LightOptions.SectionName));
-            builder.Services.AddHostedService<AuraScheduleWorker>();
-
-            builder.Services.AddSingleton<ISettingsFileProvider>(x => new SettingsFileProvider(x.GetRequiredService<ILogger<SettingsFileProvider>>(), settingsPath));
-            builder.Services.AddSingleton<MainWindow>();
-            builder.Services.AddSingleton<SettingsViewModel>();
-
-            builder.Logging.ClearProviders();
-
-            builder.Logging.AddObservableLogger();
-
-            _host = builder.Build();
-
-
-            AppDomain.CurrentDomain.UnhandledException += (sender, args) =>
-            {
-                var loggerFactory = _host.Services.GetRequiredService<ILoggerFactory>();
-                var logger = loggerFactory.CreateLogger<App>();
-
-                logger.LogError(args.ExceptionObject as Exception, "An unhandled error occurred");
-            };
+            InitializeComponent();
         }
 
         protected override void OnStartup(StartupEventArgs e)
         {
-            base.OnStartup(e);
-
             ThemeManager.Current.ThemeSyncMode = ThemeSyncMode.SyncWithAppMode;
             ThemeManager.Current.SyncTheme();
-        }
 
-        private async void Application_Startup(object sender, StartupEventArgs e)
-        {
-            await _host.StartAsync();
-
-            var mainWindow = _host.Services.GetRequiredService<MainWindow>();
-            mainWindow.Show();
-        }
-
-        private async void Application_Exit(object sender, ExitEventArgs e)
-        {
-            using (_host)
-            {
-                await _host.StopAsync();
-            }
+            base.OnStartup(e);
         }
     }
 }
---
+++ AuraScheduler.UI/Program.cs
@@ -0,0 +1,95 @@
+﻿using System.IO;
+using System.Reflection;
+using System.Windows;
+
+using AuraScheduler.UI.Infrastructure;
+using AuraScheduler.Worker;
+
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace AuraScheduler.UI
+{
+    public class Program
+    {
+        private static IHost? _host;
+
+        private const string LightSettingsFileName = "LightSettings.json";
+
+        [STAThread]
+        public static void Main(string[]? args = null)
+        {
+            var builder = Host.CreateApplicationBuilder(args);
+
+            string settingsPath;
+
+            if (builder.Environment.IsProduction())
+            {
+                var company = typeof(App).Assembly.GetCustomAttribute<AssemblyCompanyAttribute>()!.Company;
+                var product = typeof(App).Assembly.GetCustomAttribute<AssemblyProductAttribute>()!.Product;
+                var appDataRoot = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+
+                settingsPath = Path.Combine(appDataRoot, company, product, LightSettingsFileName);
+            }
+            else
+            {
+                settingsPath = LightSettingsFileName;
+            }
+
+
+            builder.Configuration.AddJsonFile(settingsPath, true, true);
+
+            builder.Services.Configure<LightOptions>(builder.Configuration.GetSection(LightOptions.SectionName));
+            builder.Services.AddHostedService<AuraScheduleWorker>();
+
+            builder.Services.AddSingleton<ISettingsFileProvider>(x => new SettingsFileProvider(x.GetRequiredService<ILogger<SettingsFileProvider>>(), settingsPath));
+
+            builder.Services.AddSingleton<App>();
+            builder.Services.AddSingleton<MainWindow>();
+            builder.Services.AddSingleton<SettingsViewModel>();
+
+            builder.Logging.ClearProviders();
+
+            builder.Logging.AddObservableLogger();
+
+            _host = builder.Build();
+
+            AppDomain.CurrentDomain.UnhandledException += CurrentDomain_UnhandledException;
+
+            var app = _host.Services.GetRequiredService<App>();
+
+            app.Startup += Application_Startup;
+            app.Exit += Application_Exit;
+
+            app.Run();
+        }
+
+        private static void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e)
+        {
+            var loggerFactory = _host!.Services.GetRequiredService<ILoggerFactory>();
+            var logger = loggerFactory.CreateLogger<App>();
+
+            logger.LogError(e.ExceptionObject as Exception, "An unhandled error occurred");
+        }
+
+        private static async void Application_Startup(object sender, StartupEventArgs e)
+        {
+            await _host!.StartAsync();
+
+            var mainWindow = _host.Services.GetRequiredService<MainWindow>();
+
+            mainWindow.Show();
+        }
+
+        private static async void Application_Exit(object sender, ExitEventArgs e)
+        {
+            using (_host)
+            {
+                await _host!.StopAsync();
+            }
+        }
+
+    }
+}
